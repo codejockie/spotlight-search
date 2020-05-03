489d00027482a95f82613b6c7f7132a468e02e70
John C. Kennedy <devjckennedy@gmail.com>
2020-05-04 01:57:01 +0900

    implement keyboard event helper

---
+++ src/helpers/keyboard.js
@@ -0,0 +1,13 @@
+export const KEY = {
+  UP: "Up",
+  ESC: "Esc",
+  DOWN: "Down",
+  LEFT: "Left",
+  ENTER: "Enter",
+  RIGHT: "Right",
+  ESCAPE: "Escape",
+  ARROW_UP: "ArrowUp",
+  ARROW_DOWN: "ArrowDown",
+  ARROW_LEFT: "ArrowLeft",
+  ARROW_RIGHT: "ArrowRight",
+}
