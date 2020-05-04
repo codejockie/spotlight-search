5bb1b526126664255bc4d10d77588a7b8b02fdbb
John C. Kennedy <devjckennedy@gmail.com>
2020-05-04 16:50:55 +0900

    add some app icons

--- src/components/SearchResult.js
+++ src/components/SearchResult.js
@@ -5,34 +5,44 @@ import "./SearchResult.scss"
 
 const SearchResult = (props) => {
   const app = useContext(AppContext)
-  const classNames = (active, index) => classnames({
-    "search-result__results-list_list-item": true,
-    "active": app.selectedItemIndex === 0 ? app.selectedItemIndex === index : active
-  })
+  const classNames = (active, index) =>
+    classnames({
+      "search-result__results-list_list-item": true,
+      active:
+        app.selectedItemIndex === 0 ? app.selectedItemIndex === index : active,
+    })
 
   return (
     <div className="search-result">
       {/* Results List */}
       <div className="search-result__results-list" onKeyDown={props.onKeyDown}>
         <ul>
-          {
-            app.searchResults &&
+          {app.searchResults &&
             app.searchResults.map((searchResult) => (
-              <li className="search-result__results-list__category" key={searchResult.name}>
-                <div className="search-result__results-list__header">{searchResult.name}</div>
+              <li
+                className="search-result__results-list__category"
+                key={searchResult.name}
+              >
+                <div className="search-result__results-list__header">
+                  {searchResult.name}
+                </div>
                 <ul>
-                  {
-                    searchResult.items.map((item, index) => (
-                      <li className={classNames(item.active, index)} key={item.name}>
-                        <img className="search-result__results-list_list-item__item-icon" src="" alt="" />
-                        {item.name}
-                      </li>
-                    ))
-                  }
+                  {searchResult.items.map((item, index) => (
+                    <li
+                      className={classNames(item.active, index)}
+                      key={item.name}
+                    >
+                      <img
+                        className="search-result__results-list_list-item__item-icon"
+                        src={`${process.env.PUBLIC_URL}/icons/${item.icon}`}
+                        alt=""
+                      />
+                      {item.name}
+                    </li>
+                  ))}
                 </ul>
               </li>
-            ))
-          }
+            ))}
           {/* <li className="search-result__results-list__category">
             <div className="search-result__results-list__header">Top Hit</div>
             <ul>
@@ -45,8 +55,7 @@ const SearchResult = (props) => {
         </ul>
       </div>
       {/* Results Details */}
-      <div className="search-result__results-detail">
-      </div>
+      <div className="search-result__results-detail"></div>
     </div>
   )
 }
